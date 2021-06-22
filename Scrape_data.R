5a3239a51a88d850dcca2ab9633d49e7ad06d60a
Joe Patrick <joseph.patrick1317@gmail.com>
2021-06-22 17:08:38 -0400

    Update Scrape_data.R

--- Scrape_data.R
+++ Scrape_data.R
@@ -1,1 +1,14 @@
+## Scraping the presidential state of the union addresses
 
+#load libraries
+library(tidyverse) 
+library(rvest)
+
+#package to get SOTU addresses is:
+library(sotu)
+
+#make dataset
+sotu <- sotu_meta
+
+#connect metadata with text
+sotu$text <- sotu_text
