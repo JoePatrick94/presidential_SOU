8e8f5cd126ebe9d30bfbe1fd93e5920f23682562
Joe Patrick <joseph.patrick1317@gmail.com>
2021-07-03 10:57:45 -0400

    update analysis

--- OneWordAnalysis.R
+++ OneWordAnalysis.R
@@ -4,7 +4,7 @@
 library(tidytext)
 
 #Read in data
-source("C:/Users/599126/Documents/Personal/presidential_SOU/GetSOTUdata.R")
+sotu <- fread("sotu_final.csv")
 
 #tokenise dataframe
 singleword <- sotu %>% unnest_tokens(word, text)
@@ -20,5 +20,27 @@ sw_nostop %>% count(word, sort = T) %>% #war is number 8... above peace...
   coord_flip()
 
 
+#tf-idf
+speechwords <- singleword %>%
+  count(president, word, sort = T)
 
+totalwords <- speechwords %>%
+  group_by(president) %>%
+  summarise(total = sum(n))
+
+speechwords <- left_join(speechwords, totalwords)
+
+speech_tf_idf <- speechwords %>%
+  bind_tf_idf(word, president, n)
+
+#visualize biden, trump, obama, bush, clinton
+speech_tf_idf %>%
+  group_by(president) %>%
+  filter(president %in% c("Biden", "Donald J. Trump", "Barack Obama", "George W. Bush", "William J. Clinton")) %>%
+  slice_max(tf_idf, n = 15) %>%
+  ungroup() %>%
+  ggplot(aes(tf_idf, fct_reorder(word, tf_idf), fill = president)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~president, ncol = 2, scales = "free") +
+  labs(x = "tf-idf", y = NULL)
 
