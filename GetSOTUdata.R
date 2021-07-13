a8c64f805198f5dbd9b124159a16f3358d87d405
Joe Patrick <joseph.patrick1317@gmail.com>
2021-07-12 20:54:53 -0400

    cleaned up sotu processing

--- GetSOTUdata.R
+++ GetSOTUdata.R
@@ -6,12 +6,10 @@ library(rvest)
 library(lubridate)
 library(data.table)
 
-#set working directory
 setwd("~/Personal/presidential_SOU")
 
 #Function to get the years active of a president
 get_years_active <- function(SOTU_pagelinks){
-  
   sotupage <- read_html(SOTU_pagelinks)
   years_active <- sotupage %>% 
     html_nodes(".dates") %>%
@@ -23,7 +21,9 @@ get_years_active <- function(SOTU_pagelinks){
 #Function to get the political party of president
 get_party <- function(presidential_biolinks){
   biopage <- read_html(presidential_biolinks)
-  party <- biopage %>% html_nodes("#block-system-main :nth-child(9)") %>% html_text()
+  party <- biopage %>% 
+    html_nodes("#block-system-main :nth-child(9)") %>% 
+    html_text()
   return(party)
 }
 
@@ -43,49 +43,52 @@ get_speech <- function(SOTU_pagelinks){
 
 sotu_df <- data.frame()
 
-for (page_number in seq(from = 0, to = 9,by=1)) {
+start_page <- 0
+end_page <- 9
 
-#State of the Union page that has all the state of the union addresses
-SOTUlistpage <- read_html(paste("https://www.presidency.ucsb.edu/documents/app-categories/spoken-addresses-and-remarks/presidential/state-the-union-addresses?page=",page_number, sep = ""))
-
-#Get president name
-president <- SOTUlistpage %>% 
-  html_nodes(".margin-top a") %>%
-  html_text()
-
-#Get year of SOTU speech
-year <- SOTUlistpage %>% 
-  html_nodes(".date-display-single") %>%
-  html_text() %>%
-  str_sub(-4) %>% 
-  as.numeric()
-
-#Get years active of president
-SOTU_pagelinks <- SOTUlistpage %>%
-  html_nodes(".field-title a") %>%
-  html_attr("href") %>% 
-  paste("https://www.presidency.ucsb.edu",., sep="")
-
-years_active <- sapply(SOTU_pagelinks, get_years_active)
-
-#Get the party part of the president
-presidential_biolinks <- SOTUlistpage %>%
-  html_nodes(".margin-top a") %>%
-  html_attr("href") %>%
-  paste("https://www.presidency.ucsb.edu",., sep="")
-
-party <- sapply(presidential_biolinks, get_party)
-
-
-#Ge the SOTU text
-text <- sapply(SOTU_pagelinks, get_speech)
-
-
-#Combine rows to the final dataset for each loop
-sotu_df <- rbind(sotu_df, data.frame(president, year, years_active, party, speech_type = "speech", text, stringsAsFactors = F))
-
-print(paste("page number:", page_number))
-
+for (page_number in start_page:end_page) {
+  
+  #State of the Union page that has all the state of the union addresses
+  SOTUlistpage <- read_html(paste("https://www.presidency.ucsb.edu/documents/app-categories/spoken-addresses-and-remarks/presidential/state-the-union-addresses?page=",page_number, sep = ""))
+  
+  #Get president name
+  president <- SOTUlistpage %>% 
+    html_nodes(".margin-top a") %>%
+    html_text()
+  
+  #Get year of SOTU speech
+  year <- SOTUlistpage %>% 
+    html_nodes(".date-display-single") %>%
+    html_text() %>%
+    str_sub(-4) %>% #take last 4 digits
+    as.numeric()
+  
+  #Get years active of president
+  SOTU_pagelinks <- SOTUlistpage %>%
+    html_nodes(".field-title a") %>%
+    html_attr("href") %>% 
+    paste("https://www.presidency.ucsb.edu",., sep="")
+  
+  years_active <- sapply(SOTU_pagelinks, get_years_active)
+  
+  #Get the party part of the president
+  presidential_biolinks <- SOTUlistpage %>%
+    html_nodes(".margin-top a") %>%
+    html_attr("href") %>%
+    paste("https://www.presidency.ucsb.edu",., sep="")
+  
+  party <- sapply(presidential_biolinks, get_party)
+  
+  
+  #Ge the SOTU text
+  text <- sapply(SOTU_pagelinks, get_speech)
+  
+  
+  #Combine rows to the final dataset for each loop
+  sotu_df <- rbind(sotu_df, data.frame(president, year, years_active, party, speech_type = "speech", text, stringsAsFactors = F))
+  
+  print(paste("page number:", page_number))
+  
 }
 
 
